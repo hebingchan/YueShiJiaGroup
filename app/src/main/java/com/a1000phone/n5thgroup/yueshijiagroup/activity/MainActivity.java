6b0f31b25106a5a9fd00b78dfc6bafcce8fb979d
hebin <hebingchan@live.com>
2016-12-28 19:30:24 +0800

    Initial commit

---
+++ app/src/main/java/com/a1000phone/n5thgroup/yueshijiagroup/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.a1000phone.n5thgroup.yueshijiagroup.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.a1000phone.n5thgroup.yueshijiagroup.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
