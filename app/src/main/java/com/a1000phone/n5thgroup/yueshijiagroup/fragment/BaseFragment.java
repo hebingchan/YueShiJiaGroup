ab54fbe70bc488316e25c145117408a5d63eaf22
hebin <hebingchan@live.com>
2016-12-29 09:34:18 +0800

    提交了BaseFragment和MineFragment

---
+++ app/src/main/java/com/a1000phone/n5thgroup/yueshijiagroup/fragment/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.a1000phone.n5thgroup.yueshijiagroup.fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by hebin on 2016/12/29.
+ */
+
+public class BaseFragment extends Fragment {
+    
+}
---
+++ app/src/main/java/com/a1000phone/n5thgroup/yueshijiagroup/fragment/MineFragment.java
@@ -0,0 +1,16 @@
+package com.a1000phone.n5thgroup.yueshijiagroup.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by hebin on 2016/12/29.
+ */
+
+public class MineFragment extends BaseFragment {
+
+}
