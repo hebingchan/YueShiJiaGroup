e97f20340c1cc1ed605ccbd27877031412949b50
hebin <hebingchan@live.com>
2017-01-03 16:26:29 +0800

    自定义ExpandableView,以及圆形ImageView

--- app/src/main/java/com/a1000phone/n5thgroup/yueshijiagroup/activity/MainActivity.java
+++ app/src/main/java/com/a1000phone/n5thgroup/yueshijiagroup/activity/MainActivity.java
@@ -2,15 +2,11 @@ package com.a1000phone.n5thgroup.yueshijiagroup.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.NestedScrollView;
-import android.util.Log;
-import android.widget.ImageView;
 
 import com.a1000phone.n5thgroup.yueshijiagroup.R;
 import com.a1000phone.n5thgroup.yueshijiagroup.base.BaseActivity;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class MainActivity extends BaseActivity {
 
